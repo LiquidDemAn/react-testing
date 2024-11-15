2c6e6b90a446c0c991dff96e618152459d2c95a1
Dmytro Shynkarenko <d.o.shynkarenko@gmail.com>
2024-11-15 21:46:30 +0200

    Group test by using describe method

--- src/components/Greet/Greet.test.tsx
+++ src/components/Greet/Greet.test.tsx
@@ -1,8 +1,10 @@
 import { render, screen } from "@testing-library/react";
 import Greet from "components/Greet";
 
-test("Greet renders correctly", () => {
-  render(<Greet />);
-  const textElement = screen.getByText(/hello/i);
-  expect(textElement).toBeInTheDocument();
+describe("Greet", () => {
+  test("renders correctly", () => {
+    render(<Greet />);
+    const textElement = screen.getByText(/hello/i);
+    expect(textElement).toBeInTheDocument();
+  });
 });
