5c9389371533deb3f8a2dddaf2d285f9d9e50f83
Dmytro Shynkarenko <d.o.shynkarenko@gmail.com>
2024-11-19 22:36:03 +0200

    getByPlaceholderText method

--- src/components/Application/Application.test.tsx
+++ src/components/Application/Application.test.tsx
@@ -21,6 +21,9 @@ describe("Application", () => {
     const nameElementTwo = screen.getByLabelText("Name", { selector: "input" });
     expect(nameElementTwo).toBeInTheDocument();
 
+    const nameElementThree = screen.getByPlaceholderText("Fullname");
+    expect(nameElementThree).toBeInTheDocument();
+
     const bioElement = screen.getByRole("textbox", { name: "Bio" });
     expect(bioElement).toBeInTheDocument();
 
