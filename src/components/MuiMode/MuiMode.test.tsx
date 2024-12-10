30e06ec5e6ceb6f7de05c8afe70d8a54885e5d74
Dmytro Shynkarenko <d.o.shynkarenko@gmail.com>
2024-12-10 22:35:00 +0200

    custom render function

--- src/components/MuiMode/MuiMode.test.tsx
+++ src/components/MuiMode/MuiMode.test.tsx
@@ -1,10 +1,9 @@
-import { render, screen } from "@testing-library/react";
 import MuiMode from "./MuiMode";
-import AppProviders from "components/Providers/AppProviders";
+import { render, screen } from "utils/test-utils";
 
 describe("MuiMode", () => {
   test("renders text correctly", () => {
-    render(<MuiMode />, { wrapper: AppProviders });
+    render(<MuiMode />);
 
     const headingElement = screen.getByRole("heading");
     expect(headingElement).toHaveTextContent("dark mode");
