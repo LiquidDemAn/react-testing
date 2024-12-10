f5895c5f012450da8482f42d95dc8a2bbb8f03cf
Dmytro Shynkarenko <d.o.shynkarenko@gmail.com>
2024-12-10 22:09:18 +0200

    keyboard interactions

--- src/components/Counter/Counter.tsx
+++ src/components/Counter/Counter.tsx
@@ -1,16 +1,27 @@
-import { useState } from "react";
+import { ChangeEvent, useState } from "react";
 
 const Counter = () => {
   const [count, setCount] = useState(0);
+  const [amount, setAmount] = useState(0);
 
   const handleClick = () => {
     setCount((prev) => prev + 1);
+  };
+
+  const onChangeAmount = (e: ChangeEvent<HTMLInputElement>) => {
+    setAmount(parseInt(e.target.value));
+  };
+
+  const handleSetAmount = () => {
+    setCount(amount);
   };
 
   return (
     <div>
       <h1>{count}</h1>
       <button onClick={handleClick}>Increment</button>
+      <input type="number" name="amount" onChange={onChangeAmount} />
+      <button onClick={handleSetAmount}>Set</button>
     </div>
   );
 };
