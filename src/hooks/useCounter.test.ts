549651923e9a993ebc89373f9fa01e8fe2b0123b
Dmytro Shynkarenko <d.o.shynkarenko@gmail.com>
2025-01-12 17:45:16 +0200

    act utility

--- src/hooks/useCounter.test.ts
+++ src/hooks/useCounter.test.ts
@@ -1,5 +1,6 @@
 import { renderHook } from "utils/test-utils";
 import { useCounter } from "./useCounter";
+import { act } from "@testing-library/react";
 
 describe("useCounter", () => {
   test("should render the initial count", () => {
@@ -15,5 +16,17 @@ describe("useCounter", () => {
 
     const { count } = result.current;
     expect(count).toBe(10);
+  });
+
+  test("should increment the count", () => {
+    const { result } = renderHook(useCounter);
+    act(() => result.current.increment());
+    expect(result.current.count).toBe(1);
+  });
+
+  test("should decrement the count", () => {
+    const { result } = renderHook(useCounter);
+    act(() => result.current.decrement());
+    expect(result.current.count).toBe(-1);
   });
 });
